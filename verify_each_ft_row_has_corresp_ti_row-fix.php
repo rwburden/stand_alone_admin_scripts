ca515addd57191f6ab1d62e67e0fde09fc9aef94
rwburden <richardburden@larouchepub.com>
2016-02-07 09:42:25 -0500

    Create verify_each_ft_row_has_corresp_ti_row-fix.php
    generates SQL commands to insert into taxonomy_index rows corresponding to rows in the vocabulary field tables that are not already in taxonomy_index.  This does not perform any database queries; it just prints the SQL commands used by verify_each_ft_row_has_corresp_ti_row1.php, but with "insert into taxonomy_index (column list)" prepended to each command.

---
+++ verify_each_ft_row_has_corresp_ti_row-fix.php
@@ -0,0 +1,22 @@
+<?php
+$ftl = array (6=>'authors',3=>'lead_item',2=>'content_type',7=>'media_type',5=>'campaign',4=>'categories',1=>'tags');
+
+foreach ($ftl as $key => $value)
+{
+	process_table('data',$key,$value);
+	process_table('revision',$key,$value);
+}
+
+
+function process_table ($d_or_r,$key,$value)
+
+{
+$table_name = 'field_'.$d_or_r.'_field_'.$value;
+
+$sql = "insert into taxonomy_index (nid,tid,sticky,created) select n.nid, f.field_".$value."_tid as tid, n.sticky, n.created from ".$table_name." as f left join taxonomy_index as i on f.entity_id=i.nid and f.field_".$value."_tid = i.tid cross join node as n on f.entity_id = n.nid where i.nid is null or i.tid is null;";
+
+print $sql."\n";
+	
+}
+
+?>
